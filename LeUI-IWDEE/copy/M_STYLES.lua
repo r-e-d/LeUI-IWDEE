0d3b3062d32585caa5c8e436b75317e3476a2788
Renaud Durlin <renaud.durlin@gmail.com>
2018-05-08 22:58:20 +0200

    v1.0

---
+++ LeUI-IWDEE/copy/M_STYLES.lua
@@ -0,0 +1,30 @@
+fontcolors['D'] = 'FFA9D4FF'
+fontcolors['S'] = 'FF0066FF'
+
+styles['normal']['font'] = 'NORMAL'
+styles['normal']['point'] = 13
+
+styles['parchment']['font'] = 'TOOLFONT'
+styles['parchment']['point'] = 13
+
+styles['button']['font'] = 'STONESML'
+styles['button']['point'] = 12
+
+styles['edit']['font'] = 'NORMAL'
+styles['edit']['point'] = 13
+
+styles['edit_parchment']['font'] = 'TOOLFONT'
+styles['edit_parchment']['point'] = 13
+
+styles['normal_parchment']['font'] = 'POSTANTI'
+styles['normal_parchment']['point'] = 13
+
+styles['label']['font'] = 'STONESML'
+styles['label']['point'] = 13
+
+styles['list']['font'] = 'STONESML'
+styles['list']['point'] = 13
+styles['list']['useFontZoom'] = 1
+
+styles['title']['font'] = 'SHERWOOD'
+styles['title']['point'] = 30
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# LeUI-IWDEE
+# lefreut's Enhanced UI for IWDEE
+
+The goal of this mod is to provide an improved UI for Icewind Dale Enhanced Edition games.
+
+See https://forums.beamdog.com/discussion/61571/mod-lefreuts-enhanced-ui-for-bg1ee-sod-bg2ee-and-eet/p1 for more information and screens.
+
+## Installation
+
+### Windows
+
+If the mod was previously installed, uninstall it before extracting the new version. LeUI is packaged and installed with WeiDU.
+
+To install, extract the contents of the archive into your game folder (the folder which contains the chitin.key file) and then double-click setup-LeUI-IWDEE.exe and follow the instructions on screen.
